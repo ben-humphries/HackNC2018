8b33a992041550a014a29c65fa037f680b867685
Ben Humphries <benfhumphries@gmail.com>
2018-10-06 13:26:01 -0400

    Added two way functionality to socket scripts

--- pi.py
+++ pi.py
@@ -3,6 +3,22 @@ import socket
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect(('localhost', 10000))
 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 9999))
+serversocket.listen(5)
+
 while True:
-    message = raw_input("Message: ")
-    clientsocket.send(message)
+    try:
+        connection, address = serversocket.accept()
+    except:
+        print("Searching...")
+    while True:
+
+        buf = connection.recv(64)
+        if len(buf) > 0:
+            print(buf.decode())
+            
+        message = raw_input("Message: ")
+        clientsocket.send(message)
+
+    
